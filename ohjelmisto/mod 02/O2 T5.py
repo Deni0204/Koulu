fe4e85ce920bb109eb130d70de7a68cba3db4134
dearn <dearnim@metropolia.fi>
2025-01-16 14:11:59 +0200

    Tehty

--- ohjelmisto/mod 02/O2 T5.py
+++ ohjelmisto/mod 02/O2 T5.py
@@ -1,6 +1,16 @@
-leiviska_int= int(input(" Anna leiviska "))=
-Naula_int= int(input(" anna naula"))
-Luoti_int= int(input(input( "anna luoti"))
+Leiviskä_float = float(input("anna leiviska "))
+Naula_float = float(input("anna naula "))
+Luoti_float = float(input("anna luoti "))
 
-gramma = (leiviska_int*20)
+Leiviskä_G = Leiviskä_float*20*32*13.3
+Naula_g = Naula_float*32*13.3
+Luoti_g = Luoti_float*13.3
+
+Grammojen_tulo = Leiviskä_G + Naula_g + Luoti_g
+
+Kilo_g = int(Grammojen_tulo // 1000)
+G = round(float(Grammojen_tulo - Kilo_g * 1000), 2)
+
+print(f"Kilogrammat: {Kilo_g}")
+print(f"Grammat: {G}")
 
